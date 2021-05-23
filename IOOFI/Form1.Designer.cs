1ad39c0f7ec74aaf5e6d2e4400f784270c742f8c
Yuriko Goto <gotoyuriko95@gmail.com>
2021-05-23 06:05:14 +0000

    init

---
+++ IOOFI/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Titlelbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.DetailLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Titlelbl
+            // 
+            this.Titlelbl.AutoSize = true;
+            this.Titlelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titlelbl.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Titlelbl.Location = new System.Drawing.Point(119, 85);
+            this.Titlelbl.Name = "Titlelbl";
+            this.Titlelbl.Size = new System.Drawing.Size(588, 32);
+            this.Titlelbl.TabIndex = 0;
+            this.Titlelbl.Text = "Welcome to Discussion Room Reservation";
+            this.Titlelbl.UseMnemonic = false;
+            this.Titlelbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Location = new System.Drawing.Point(242, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.button2.Location = new System.Drawing.Point(455, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Signup";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DetailLbl
+            // 
+            this.DetailLbl.AutoSize = true;
+            this.DetailLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DetailLbl.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.DetailLbl.Location = new System.Drawing.Point(283, 183);
+            this.DetailLbl.Name = "DetailLbl";
+            this.DetailLbl.Size = new System.Drawing.Size(212, 32);
+            this.DetailLbl.TabIndex = 3;
+            this.DetailLbl.Text = "Login / Signup";
+            this.DetailLbl.UseMnemonic = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DetailLbl);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Titlelbl);
+            this.Name = "Form1";
+            this.Text = "Home Screen Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Titlelbl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label DetailLbl;
+    }
+}
+
