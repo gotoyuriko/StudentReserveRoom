1d2f656cf4e41d625a82ece927eaec503fdb1589
BuildTools <unconfigured@null.spigotmc.org>
2021-05-25 20:10:45 +0700

    pekopeko

--- DiscussionRoomReservationSystem/Frm05_StudentReservation.Designer.cs
+++ DiscussionRoomReservationSystem/Frm05_StudentReservation.Designer.cs
@@ -163,6 +163,7 @@ namespace DiscussionRoomReservationSystem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.EndTimeSelectPicker);
             this.Controls.Add(this.StartTimeSelectPicker);
--- DiscussionRoomReservationSystem/Frm08_YourReservation.Designer.cs
+++ DiscussionRoomReservationSystem/Frm08_YourReservation.Designer.cs
@@ -29,12 +29,141 @@ namespace DiscussionRoomReservationSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.TitleLbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ChangeBtn = new System.Windows.Forms.Button();
+            this.RoomTitleLbl = new System.Windows.Forms.Label();
+            this.DateReservedLbl = new System.Windows.Forms.Label();
+            this.TimeReservedLbl = new System.Windows.Forms.Label();
+            this.CapacityLbl = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.BackColor = System.Drawing.Color.White;
+            this.CancelBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelBtn.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.CancelBtn.Location = new System.Drawing.Point(193, 372);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(163, 52);
+            this.CancelBtn.TabIndex = 11;
+            this.CancelBtn.Text = "Cancel Reservation";
+            this.CancelBtn.UseVisualStyleBackColor = false;
+            // 
+            // TitleLbl
+            // 
+            this.TitleLbl.AutoSize = true;
+            this.TitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLbl.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.TitleLbl.Location = new System.Drawing.Point(219, 54);
+            this.TitleLbl.Name = "TitleLbl";
+            this.TitleLbl.Size = new System.Drawing.Size(370, 32);
+            this.TitleLbl.TabIndex = 10;
+            this.TitleLbl.Text = "YOUR RESERVED ROOM";
+            this.TitleLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CapacityLbl);
+            this.groupBox1.Controls.Add(this.TimeReservedLbl);
+            this.groupBox1.Controls.Add(this.DateReservedLbl);
+            this.groupBox1.Controls.Add(this.RoomTitleLbl);
+            this.groupBox1.Location = new System.Drawing.Point(158, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(496, 239);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // ChangeBtn
+            // 
+            this.ChangeBtn.BackColor = System.Drawing.Color.White;
+            this.ChangeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChangeBtn.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.ChangeBtn.Location = new System.Drawing.Point(445, 372);
+            this.ChangeBtn.Name = "ChangeBtn";
+            this.ChangeBtn.Size = new System.Drawing.Size(167, 52);
+            this.ChangeBtn.TabIndex = 13;
+            this.ChangeBtn.Text = "Change Reservation";
+            this.ChangeBtn.UseVisualStyleBackColor = false;
+            // 
+            // RoomTitleLbl
+            // 
+            this.RoomTitleLbl.AutoSize = true;
+            this.RoomTitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RoomTitleLbl.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.RoomTitleLbl.Location = new System.Drawing.Point(149, 42);
+            this.RoomTitleLbl.Name = "RoomTitleLbl";
+            this.RoomTitleLbl.Size = new System.Drawing.Size(199, 32);
+            this.RoomTitleLbl.TabIndex = 14;
+            this.RoomTitleLbl.Text = "ROOM NAME";
+            this.RoomTitleLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // DateReservedLbl
+            // 
+            this.DateReservedLbl.AutoSize = true;
+            this.DateReservedLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DateReservedLbl.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.DateReservedLbl.Location = new System.Drawing.Point(108, 98);
+            this.DateReservedLbl.Name = "DateReservedLbl";
+            this.DateReservedLbl.Size = new System.Drawing.Size(275, 25);
+            this.DateReservedLbl.TabIndex = 15;
+            this.DateReservedLbl.Text = "Reserved Date: DD/MM/YY";
+            this.DateReservedLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TimeReservedLbl
+            // 
+            this.TimeReservedLbl.AutoSize = true;
+            this.TimeReservedLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeReservedLbl.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.TimeReservedLbl.Location = new System.Drawing.Point(82, 132);
+            this.TimeReservedLbl.Name = "TimeReservedLbl";
+            this.TimeReservedLbl.Size = new System.Drawing.Size(336, 25);
+            this.TimeReservedLbl.TabIndex = 16;
+            this.TimeReservedLbl.Text = "Reserved Time: HH:MM - HH:MM";
+            this.TimeReservedLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // CapacityLbl
+            // 
+            this.CapacityLbl.AutoSize = true;
+            this.CapacityLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CapacityLbl.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.CapacityLbl.Location = new System.Drawing.Point(147, 168);
+            this.CapacityLbl.Name = "CapacityLbl";
+            this.CapacityLbl.Size = new System.Drawing.Size(196, 25);
+            this.CapacityLbl.TabIndex = 17;
+            this.CapacityLbl.Text = "Capacity: N people";
+            this.CapacityLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Frm08_YourReservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ChangeBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.TitleLbl);
+            this.Name = "Frm08_YourReservation";
             this.Text = "Frm8_YourReservation";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Label TitleLbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button ChangeBtn;
+        private System.Windows.Forms.Label CapacityLbl;
+        private System.Windows.Forms.Label TimeReservedLbl;
+        private System.Windows.Forms.Label DateReservedLbl;
+        private System.Windows.Forms.Label RoomTitleLbl;
     }
 }
